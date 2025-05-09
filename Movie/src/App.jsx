a3560b70ba749fdd90f3cc1efbac4ef3f11b6ffd
Mukul Singhal <work.mukulsinghal@gmail.com>
2025-05-10 01:40:21 +0530

    completed header

--- Movie/src/App.jsx
+++ Movie/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Search from './components/search'
+import { useState } from 'react'
 
 const App = () => {
+
+  const [searchTerm, setSearchTerm] = useState('')
+
+
+
   return (
     <main>
+
+
 
       <div className='pattern' />
 
@@ -11,6 +20,11 @@ const App = () => {
           <img src='hero-img.png'  alt='hero img' />
           <h1>Find  <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
         </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}  />
+
+
+
 
       </div>
 
